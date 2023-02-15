76deef444cb085b9d2c28409148563191d14b597
Sushmita Ghosh <pujaghosh251096@gmail.com>
2023-02-15 20:29:17 +0530

    day 1 solving problems

--- day1.2.js
+++ day1.2.js
@@ -28,4 +28,15 @@ function printPyramid(n) {
   }
 }
 
+/*** - nextleap solution
+ * public void printPyramid(int n) {
+		for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+	}
+ */
+
 printPyramid(5);
