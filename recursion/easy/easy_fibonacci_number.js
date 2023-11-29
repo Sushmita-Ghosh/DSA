2a59178d48b4d3cfc695013368ab818c25b6ad79
Sushmita Ghosh <pujaghosh251096@gmail.com>
2023-11-28 19:06:36 -0800

    [Recursion] Fibonacci

---
+++ recursion/easy/easy_fibonacci_number.js
@@ -0,0 +1,16 @@
+// Iterative Approach
+
+var fib = function (n) {
+  let arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[n];
+};
+
+var fibRecursion = function (n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+};
---
+++ recursion/easy/fibonacci.md
@@ -0,0 +1,1 @@
+# Fibonacci Number Explanation - @RoadSideCoder YT
