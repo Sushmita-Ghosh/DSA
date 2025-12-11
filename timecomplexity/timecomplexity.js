4ae0894ed86f2e2e63a0478c1baa28e8fb669198
Sushmita Ghosh <pujaghosh251096@gmail.com>
2025-12-10 19:37:50 -0800

    Time complexity notes

---
+++ timecomplexity/timecomplexity.js
@@ -0,0 +1,8 @@
+/** What is Time Complexity?
+Time complexity measures how efficient an algorithm is as the input size increases. It's not the same as the actual time taken to run a program.
+
+
+
+What is Big o notation?
+Symbol to denote the worst time complexity of an algorithm.
+*/
