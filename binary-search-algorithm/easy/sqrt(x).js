79c2e998408ecb3cb9dbdd96eb0c5628b90f1e25
Sushmita Ghosh <pujaghosh251096@gmail.com>
2026-01-29 12:50:45 -0800

    Pow of X

--- binary-search-algorithm/easy/sqrt(x).js
+++ binary-search-algorithm/easy/sqrt(x).js
@@ -37,3 +37,55 @@ Constraints:
 var mySqrt = function (x) {
   return Math.trunc(x ** 0.5);
 };
+
+
+/** 2. USing Linear search */
+
+
+
+
+
+/** 3. Using Binary Search */
+
+// The maths: The sqrt of a number will always lie bw 2 and the number/2 - it can never exceed that
+
+/**
+ * The algorithm 
+ * run the loop from 2 to x/2
+ * find mid ele = m
+ * if(x< square(m))
+ *  r= m-1
+ * else 
+ *  l = m+1
+ * 
+ * uptill up, is pefect for perfect sqaures
+ * if not , right wil be the closest to the perfect square , so we will have to return that
+ */
+
+
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    if (x < 2) return x
+
+
+    let l = 2;
+    let r = Math.ceil(x / 2)
+
+    while (l <= r) {
+        let m = Math.ceil((l + r) / 2)
+        if (x === m * m) {
+            return m
+        } else if (x > m * m) {
+            l = m + 1
+        } else {
+            r = m - 1
+        }
+    }
+
+    return r;
+
+};
