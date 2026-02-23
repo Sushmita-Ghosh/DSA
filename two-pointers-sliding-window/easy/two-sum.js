c3f8451c90f8f5362a4c1434a8c5d50fc06dcff8
Sushmita Ghosh <pujaghosh251096@gmail.com>
2026-02-23 08:48:56 -0800

    Two Sum, With Variation

--- two-pointers-sliding-window/easy/two-sum.js
+++ two-pointers-sliding-window/easy/two-sum.js
@@ -1,1 +1,38 @@
 /**https://leetcode.com/problems/two-sum/description/ */
+
+//2pointer
+var twoSum = function(nums, target) {
+    let n = nums.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let sum = nums[i] + nums[j];
+            if (sum === target) {
+                return [i, j];
+            }
+        }
+    }
+}  
+
+
+// Using hashmap
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(arr, target) {
+    let n = arr.length
+    let map = {}
+
+    for(let i =0; i<n ; i++){
+        map[arr[i]] = i // storing the indexes in arr
+    }
+
+    for(let i =0; i<n ; i++){
+        let pairToFind = target - arr[i]
+        /** if it matches the index */
+        if(map[pairToFind] && map[pairToFind]!== i){
+            return [i, map[pairToFind]]
+        }
+    }
+};
